dff438b679ff1257dddb67fe0e5ff7c353970ffc
the-web-girl <angelique@web-i.org>
2025-06-13 15:44:56 +0200

    Revert "ajout d'une ligne supplémentaire à Shipping_Cost_Calculator.py"
    This reverts commit d1869f039f793b98bb1691aa650c4da16588600f.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,4 +11,3 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
      # Voici une nouvelle mise à jour par https://github.com/the-web-girl
-     # Voici une autre mise à jour de https://github.com/the-web-girl
